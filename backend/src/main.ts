facb48ca75f84e78bda163a59f2be1bd2f3b414d
L20660042 <l20660042@matehuala.tecnm.mx>
2025-03-29 23:42:43 -0600

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,19 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Configurar CORS con la URL del frontend en Vercel
+  // Configura CORS para que tu backend acepte peticiones desde el frontend
   app.enableCors({
-    origin: process.env.FRONTEND_URL || '*',
+    origin: process.env.FRONTEND_URL || 'https://l20660042.github.io/Frontend/',  // Usar la URL de tu frontend en producción
     methods: 'GET, POST, PUT, DELETE',
     allowedHeaders: 'Content-Type, Authorization',
   });
 
-  // Usar el puerto que asigna Vercel o 3000 en local
-  const PORT = process.env.PORT || 3000;
-  await app.listen(PORT);
+  await app.listen(3000);
 }
-
 bootstrap();
