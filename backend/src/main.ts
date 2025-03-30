fe83af3b55caa1839b4fa16a389adeba011176d2
L20660042 <l20660042@matehuala.tecnm.mx>
2025-03-30 00:16:57 -0600

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,17 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
-
-dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  // Configura CORS para que tu backend acepte peticiones desde el frontend
+  
+  // Habilitar CORS con el dominio específico de tu frontend
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'https://l20660042.github.io/Frontend/',  // Usar la URL de tu frontend en producción
-    methods: 'GET, POST, PUT, DELETE',
-    allowedHeaders: 'Content-Type, Authorization',
+    origin: 'https://l20660042.github.io/Frontend', // URL de tu frontend
+    methods: 'GET, POST, PUT, DELETE', // Métodos permitidos
+    allowedHeaders: 'Content-Type, Authorization', // Encabezados permitidos
   });
 
   await app.listen(3000);
