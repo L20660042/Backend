23b0f4aed453bb1371b0caa5ea181f828bfe415a
L20660042 <l20660042@matehuala.tecnm.mx>
2025-04-03 13:58:43 -0600

    upp

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,14 @@
+// src/app.module.ts
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService], // Registra el servicio aquí
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://tomas:tomas@cluster0.ztveb.mongodb.net/NuevoProyecto'),
+    UsersModule,
+    AuthModule
+  ],
 })
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,16 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+
+const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  
-  // Habilitar CORS con el dominio específico de tu frontend
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
   app.enableCors({
-    origin: ['https://l20660042.github.io'], // URL de tu frontend
-    methods: 'GET, POST, PUT, DELETE', // Métodos permitidos
-    allowedHeaders: 'Content-Type, Authorization', // Encabezados permitidos
+    origin: 'https://l20660042.github.io',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
   });
 
-  await app.listen(3000);
+  await app.init();
 }
+
 bootstrap();
+
+export default server;
