938ca3e2fa388d91fc8f9763e1bc0e66987603f9
L20660042 <l20660042@matehuala.tecnm.mx>
2025-04-03 20:41:02 -0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+
+const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  
   app.enableCors({
-    origin: 'https://l20660042.github.io',
+    origin: '*', // o reemplaza con el dominio correcto
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
 
-  await app.listen(process.env.PORT || 3000);
+  const port = parseInt(process.env.PORT, 10) || 3000;
+  await app.listen(port, () => {
+    console.log(`🚀 App corriendo en el puerto ${port}`);
+  });
 }
 
 bootstrap();
+
+export default server;
