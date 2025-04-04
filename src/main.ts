e5246f92ea4810b16a010e8c39b4bdb4c9ba87c7
L20660042 <l20660042@matehuala.tecnm.mx>
2025-04-03 20:25:13 -0600

    up

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://l20660042.github.io', // o '*' para pruebas
+    origin: 'https://l20660042.github.io',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
