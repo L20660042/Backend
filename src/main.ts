53fc51253af4aa86f2f078c3f4d4efbffba8aa6a
L20660042 <l20660042@matehuala.tecnm.mx>
2025-05-06 23:28:25 -0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,11 @@ async function bootstrap() {
   
   // Habilitar CORS para todos los orígenes
   app.enableCors({
-    origin: [
-      'https://l20660042.github.io', // frontend
-      'https://iaemocion-production.up.railway.app:8000', // IA backend
-    ],
+    origin: 'https://l20660042.github.io', // El dominio de tu frontend
     methods: 'GET,POST,PUT,DELETE',
     credentials: true, // si usas cookies o auth
   });
-  app.use(json({ limit: '10mb' }));
+  
   await app.listen(3000);
 }
 bootstrap();
