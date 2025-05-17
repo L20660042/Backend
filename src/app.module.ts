253f42ee5678b7a73d3220246153d734abf8ab4e
L20660042 <l20660042@matehuala.tecnm.mx>
2025-05-17 12:52:13 -0600

    des

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,24 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
-
-@Module({
+Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    
+
     MongooseModule.forRoot('mongodb+srv://tomas:tomas@cluster0.ztveb.mongodb.net/NuevoProyecto'),
+
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'uploads'),
+      serveRoot: '/uploads',
+    }),
+
     UsersModule,
-    AuthModule
+    AuthModule,
   ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { json } from 'express';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  
-  // Habilitar CORS para todos los orígenes
-  app.enableCors({
-    origin: 'https://l20660042.github.io', // El dominio de tu frontend
-    methods: 'GET,POST,PUT,DELETE',
-    credentials: true, // si usas cookies o auth
-  });
-  
+  const app = await NestFactory.create<NestExpressApplication>(AppModule); // <- OJO aquí
+
+app.enableCors({
+  origin: [
+    'https://l20660042.github.io',
+    'http://localhost:3000'
+  ],
+  methods: 'GET,POST,PUT,DELETE',
+  credentials: true,
+});
+
   await app.listen(3000);
 }
 bootstrap();
