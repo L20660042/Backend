ade1aba73a3e79322c5a95b77859f71039bbd438
L20660042 <l20660042@matehuala.tecnm.mx>
2025-05-17 13:56:46 -0600

    update

--- src/users/dto/emotion-analysis.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsNotEmpty } from 'class-validator';
-
-export class AnalyzeEmotionDto {
-  @IsNotEmpty()
-  userId: string;
-
-  // La imagen se enviará como un archivo en la solicitud HTTP
-}
